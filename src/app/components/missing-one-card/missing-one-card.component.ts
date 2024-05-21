13b95ebacd6bd9f3e59bea14ac123ed1922b6ae2
Iván Scarsella <scarsellaivan@gmail.com>
2024-05-21 14:14:10 -0300

    update home

---
+++ src/app/components/missing-one-card/missing-one-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-missing-one-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './missing-one-card.component.html',
+  styles: ``
+})
+export class MissingOneCardComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component'
+import { MissingOneCardComponent } from '../../components/missing-one-card/missing-one-card.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, MissingOneCardComponent],
   templateUrl: './home.component.html',
   styles: ``
 })
