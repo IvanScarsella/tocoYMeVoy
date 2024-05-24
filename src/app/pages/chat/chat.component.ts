5a4b027083fce7311fdcb11bcbd25b0772380199
Iván Scarsella <scarsellaivan@gmail.com>
2024-05-24 14:22:05 -0300

    add chat page

--- src/app/pages/chat/chat.component.ts
+++ src/app/pages/chat/chat.component.ts
@@ -10,4 +10,12 @@ import { NavbarComponent } from '../../components/navbar/navbar.component';
 })
 export class ChatComponent {
 
+  contactsList: any[] = [
+    { image: '../../../assets/profile1.jpg', name: 'Santiago Flores', lastMessage: 'Coordina con tu nuevo compañero' },
+    { image: '../../../assets/friend3.png', name: 'Iván Riquelme', lastMessage: 'Dale dale, nos vemos!!' },
+    { image: '../../../assets/friend2.png', name: 'Sofía Amarilla', lastMessage: 'Visto el domingo' },
+    { image: '../../../assets/friend1.png', name: 'Julián Baldaro', lastMessage: 'Nos vemos capo' },
+    { image: '../../../assets/friend5.png', name: 'Marcelo Peralta', lastMessage: 'Visto la semana pasada' },
+  ]
+
 }
