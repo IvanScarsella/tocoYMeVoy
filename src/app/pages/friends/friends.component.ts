95f88b8948603170bbd14f090aefc367cf16976f
Iván Scarsella <scarsellaivan@gmail.com>
2024-05-24 12:02:40 -0300

    add friends page

--- src/app/pages/friends/friends.component.ts
+++ src/app/pages/friends/friends.component.ts
@@ -1,16 +1,22 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
+// import friend1 from '../../../assets/friend1.png';
 
 @Component({
   selector: 'app-friends',
   standalone: true,
-  imports: [],
-  template: `
-    <p>
-      friends works!
-    </p>
-  `,
+  imports: [NavbarComponent, RouterLink, RouterOutlet],
+  templateUrl: './friends.component.html',
   styles: ``
 })
 export class FriendsComponent {
+
+  friendsList: any[] = [
+    { image: '../../../assets/friend1.png', name: 'Julián Baldaro' },
+    { image: '../../../assets/friend2.png', name: 'Sofía Amarilla' },
+    { image: '../../../assets/friend3.png', name: 'Iván Riquelme' },
+    { image: '../../../assets/friend4.png', name: 'Fernando Marín' },
+  ]
 
 }
