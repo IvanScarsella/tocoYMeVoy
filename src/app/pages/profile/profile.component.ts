b324ce65c7c0b620fef0a6fe3c73490a2c7b0960
Iván Scarsella <scarsellaivan@gmail.com>
2024-05-28 17:41:33 -0300

    profile finished

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, RouterLink, RouterOutlet],
   templateUrl: './profile.component.html',
   styles: ``
 })
